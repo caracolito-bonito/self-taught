dc02a276e398dd05cf52d21a9084eb537f2f55f9
Sergey Kupriyanov <xvoztuk@gmail.com>
2026-09-14 23:56:28 +0400

    experimenting with deduplicating business event, not position

---
+++ courses/kafka/labs/examples/09_business_event.rs
@@ -0,0 +1,78 @@
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use rdkafka::{
+    ClientConfig, Message,
+    producer::{FutureProducer, FutureRecord},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct OrderEvent {
+    event_id: String,
+    order_id: String,
+    event_type: EventType,
+}
+
+#[derive(Serialize, Deserialize)]
+enum EventType {
+    OrderCreated,
+    PaymentAuthorized,
+    OrderShipped,
+}
+
+const TIMEOUT: Duration = Duration::from_secs(5);
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let topic = "order-events";
+
+    let event = OrderEvent {
+        event_id: String::from("evt-42"),
+        order_id: String::from("order-1001"),
+        event_type: EventType::OrderCreated,
+    };
+
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .set("message.timeout.ms", "5000")
+        .create()?;
+
+    let event_serialized = serde_json::to_string(&event)?;
+
+    for _ in 0..2 {
+        let record = FutureRecord::to(topic)
+            .key(&event.order_id)
+            .payload(&event_serialized);
+
+        let sent = producer.send(record, TIMEOUT).await;
+
+        match sent {
+            Ok(delivered) => println!(
+                "Sent! Event_id: {} to the topic: {} P{}/O{}",
+                event.event_id, topic, delivered.partition, delivered.offset
+            ),
+            Err((kafkar_err, message)) => {
+                let key = message.key();
+
+                match key {
+                    Some(key) => {
+                        return Err(kafkar_err).context(format!(
+                            "Delivery to the topic: {} for key: {} failed",
+                            message.topic(),
+                            String::from_utf8_lossy(key)
+                        ));
+                    }
+                    None => {
+                        return Err(kafkar_err).context(format!(
+                            "Delivery to the topic: {} failed. Key is absent",
+                            message.topic()
+                        ));
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
